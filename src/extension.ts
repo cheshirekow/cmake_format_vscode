ea11f803a401f7c0b954d74084482dae3060c086
Josh Bialkowski <jbialkowski@nvidia.com>
2018-09-12 20:51:43 -0700

    more cleanup

--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,10 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
     console.log('"cmake-format" extension is now active!');
 
-    // 👍 formatter implemented using API
     let disposable = vscode.languages.registerDocumentFormattingEditProvider(
         { scheme: 'file', language: 'cmake' }, {
             provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
@@ -33,6 +26,5 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {
 }
